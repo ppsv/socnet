3221a0ee16b57189bcc078390c049bd91d9b33d9
ppsv <ppsv-7@ya.ru>
2020-07-12 20:34:44 +0300

    18 lesson

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import s from './Header.module.css';
+import s from '../Dialogs/Dialogs.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-            <img src='https://seeklogo.com/images/F/flag-map-of-belarus-logo-81E1C60786-seeklogo.com.png'/>
+        <img src='https://seeklogo.com/images/F/flag-map-of-belarus-logo-81E1C60786-seeklogo.com.png'/>
     </header>
 }
 
