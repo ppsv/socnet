ebcf348005504f0e3a734fe94a451e6085332a58
ppsv <ppsv-7@ya.ru>
2020-07-13 08:30:57 +0300

    19-1

---
+++ src/components/Navbar/Setting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Setting.module.css';
+
+const Setting = () => {
+    return (
+        <div className={s.item}>
+            Music
+        </div>
+    )
+}
+
+export default Setting;
