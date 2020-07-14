5783eb14cf80f46b2b75c746b81f5156fa3a00d8
ppsv <ppsv-7@ya.ru>
2020-07-14 09:28:11 +0300

    25 lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,18 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-    let posts = []
+   
+    let posts = [
+        { message:'Hi!',like:'Like=15',dislike:'Dislike=0'},
+        { message:'Hello!', like:'Like=17', dislike:'Dislike=0'},
+        {message: 'Hi everyone!', like: 'Like=12', dislike: 'Dislike=0'},
+        {message: 'Hi!', like: 'Like=25', dislike: 'Dislike=0'},
+        {message: 'Fine!', like: 'Like=19', dislike: 'Dislike=0'},
+        {message: 'Cool!', like: 'Like=35', dislike: 'Dislike=0'}
+    ]
+
+    let postsElement = posts.map(el => <Post message={el.message} like={el.like} dislike={el.dislike}/>)
+
     return <div className={s.postsBlock}>
         <h2>MyPosts</h2>
         <div>
@@ -17,9 +28,8 @@ const MyPosts = () => {
 
         </div>
         <div className={s.posts}>
-            <Post message='Hi!' like='Like=15' dislike='Dislike=0'/>
-            <Post message='Hello!' like='Like=14' dislike='Dislike=0'/>
-            <Post message='Hi everyone!' like='Like=16' dislike='Dislike=0'/>
+            {postsElement}
+
         </div>
 
     </div>
