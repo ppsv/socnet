87fb7983d367f26a2b5f7403275a5e702b5874a8
ppsv <ppsv-7@ya.ru>
2020-07-15 13:40:53 +0300

    31 lesson

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,6 +9,13 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogsData.map(d => (<DialogItem name={d.name} id={d.id}/>));
     let messageElements = props.state.messageData.map(m => <Message message={m.message} Likes={m.likes}/>);
 
+    let newPostEl = React.createRef();
+
+    let addPost =() => {
+        let text =newPostEl.current.value;
+        alert(text);
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -16,7 +23,14 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messageElements}
+                <div>
+                    <textarea ref={newPostEl}></textarea>
+                </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
+                </div>
             </div>
+
         </div>
     )
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,18 +4,23 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-
-
     let postsElement = props.posts.map(el => <Post message={el.message} like={el.like} dislike={el.dislike}/>)
+
+    let newPostEl = React.createRef();
+
+    let addPost =() => {
+        let text =newPostEl.current.value;
+        alert(text);
+    }
 
     return <div className={s.postsBlock}>
         <h2>MyPosts</h2>
         <div>
             <div>
-                <textarea></textarea>
+                <textarea ref={newPostEl}></textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
 
 
